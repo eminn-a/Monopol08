d7dd8a365d8799187d570e32bc7592e2dffb79ad
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-03-31 19:06:09 +0300

    Add property validation added

--- monopol/src/app/properties/add-propertiy/add-propertiy.component.ts
+++ monopol/src/app/properties/add-propertiy/add-propertiy.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-add-propertiy',
   templateUrl: './add-propertiy.component.html',
-  styleUrls: ['./add-propertiy.component.css']
+  styleUrls: ['./add-propertiy.component.css'],
 })
 export class AddPropertiyComponent {
+  allFieldsError: boolean = false;
 
+  constructor(private apiService: ApiService) {}
+  addProperty(form: NgForm) {
+    if (form.invalid) {
+      this.allFieldsError = true;
+      setTimeout(() => {
+        this.allFieldsError = false;
+      }, 2000);
+      return;
+    }
+    console.log(form.value);
+  }
 }
--- monopol/src/app/properties/properties.module.ts
+++ monopol/src/app/properties/properties.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddPropertiyComponent } from './add-propertiy/add-propertiy.component';
 import { PropertyRoutingModule } from './property-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddPropertiyComponent],
-  imports: [CommonModule, PropertyRoutingModule],
+  imports: [
+    CommonModule,
+    PropertyRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class PropertiesModule {}
