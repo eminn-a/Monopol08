4f2d781df204cff0ee8c25b5a8aeccaddbdd6524
Emin A <122976155+eminn-a@users.noreply.github.com>
2024-04-01 01:17:41 +0300

    Logged guards

--- monopol/src/app/user/user-routing.module.ts
+++ monopol/src/app/user/user-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { LoggedActivate } from '../guards/logged.activate';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoggedActivate] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [LoggedActivate],
+  },
 ];
 
 @NgModule({
